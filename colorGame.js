def337ee1473d3fc9cbc1a0deb389e26ea543c29
julio.polo.arias@gmail.com <julio.polo.arias@gmail.com>
2017-07-01 08:30:13 -0500

    Added random colors funcionality and some cosmetics changes

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColor(6);
 var squares = document.querySelectorAll(".square");
-var pickColor = colors[3];
+var pickColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickColor;
 
@@ -18,9 +12,42 @@ for (var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", function(){
        var clickedColor = this.style.background;
         if (clickedColor === pickColor){
-            alert("correct!");
+            messageDisplay.textContent="Correct!";
+            changeColor(clickedColor);
+            h1.style.background = clickedColor;
         } else {
             this.style.background = "#232323";
+            messageDisplay.textContent="Try Again";
         }
     });
+}
+
+function changeColor(color){
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.background = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColor(num){
+    var arr=[];
+    for (var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    //pick a red from 0 to 255
+    //pick a green from 0 to 255
+    //pick a blue from 0 to 255
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b +")";
 }
