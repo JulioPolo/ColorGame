4ea0b457fdef18444ea09a53f0adae44d3d3748b
julio.polo.arias@gmail.com <julio.polo.arias@gmail.com>
2017-07-05 07:16:37 -0500

    Some Changes to the CSS and polishing of JS

--- colorGame.js
+++ colorGame.js
@@ -1,20 +1,25 @@
-var colors = generateRandomColor(6);
+var difficulty = 6;
+var colors = generateRandomColor(difficulty);
 var squares = document.querySelectorAll(".square");
-var pickColor = pickColor();
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
 
-colorDisplay.textContent = pickColor;
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++){
     squares[i].style.background = colors[i];
     squares[i].addEventListener("click", function(){
        var clickedColor = this.style.background;
-        if (clickedColor === pickColor){
+        if (clickedColor === pickedColor){
             messageDisplay.textContent="Correct!";
             changeColor(clickedColor);
             h1.style.background = clickedColor;
+            resetButton.textContent = "Play Again?";
         } else {
             this.style.background = "#232323";
             messageDisplay.textContent="Try Again";
@@ -22,8 +27,45 @@ for (var i = 0; i < squares.length; i++){
     });
 }
 
-function changeColor(color){
+easyButton.addEventListener("click", function(){
+    difficulty = 3;
+    reDraw(difficulty);
+    this.classList.add("selected");
+    hardButton.classList.remove("selected");
+});
+
+hardButton.addEventListener("click", function(){
+    difficulty = 6;
+    reDraw(difficulty);
+    this.classList.add("selected");
+    easyButton.classList.remove("selected");
+});
+
+resetButton.addEventListener("click", function(){
+    reDraw(difficulty);
+});
+
+function CleanSquares(){
+    for (var i = 0; i < 6; i++){
+        squares[i].style.background = "#232323";
+    }
+}
+
+function reDraw(dif) {
+    CleanSquares();
+    colors = generateRandomColor(dif);
+    pickedColor = pickColor();
     for (var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    colorDisplay.textContent = pickedColor;
+    h1.style.background = "steelblue";
+    messageDisplay.textContent = "";
+    resetButton.textContent = "New Colors";
+}
+
+function changeColor(color){
+    for (var i = 0; i < difficulty; i++){
         squares[i].style.background = color;
     }
 }
